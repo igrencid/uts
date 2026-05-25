608243d05ca4858249efc173da8f7963488a0acf
igrencid <ilhamlima903@gmail.com>
2026-05-25 17:31:49 +0700

    setup controller and gitignore

--- src/app/Http/Controllers/BiodataController.php
+++ src/app/Http/Controllers/BiodataController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Biodata;
+use App\Models\Project;
 
-class BiodataController extends Controller
+class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $biodata = Biodata::with([
+            'skills',
+            'educations'
+        ])->first();
+
+        $projects = Project::latest()->get();
+
+        return view('home', compact(
+            'biodata',
+            'projects'
+        ));
+    }
 }
